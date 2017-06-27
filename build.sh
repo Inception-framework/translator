350ae2c67be29788f95787d81c5bcb78fc2e5836
Giovanni <camurati@eurecom.fr>
2017-06-27 11:34:00 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@
 --disable-jit \
 --disable-libffi \
 --enable-debug-symbols \
---with-llvmsrc=/home/zer0day/Projects/Inception/tools/llvm/llvm3.6 \
---with-llvmobj=/home/zer0day/Projects/Inception/tools/llvm/llvm3.6/build_u16 \
+-with-llvmsrc=/home/giovanni/phd/Inception/tools/llvm/llvm3.6/ \
+--with-llvmobj=/home/giovanni/phd/Inception/tools/llvm/llvm3.6/ \
 --disable-optimized
 
 make -j16
+
+sudo make install
