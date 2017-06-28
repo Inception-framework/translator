16929ea724c908525074e6cc1d0c475882969e4a
Giovanni <camurati@eurecom.fr>
2017-06-28 09:14:39 +0200

    paths must be absolute

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 #./autoconf/AutoRegen.sh
 
 ./configure --disable-docs \
@@ -6,8 +8,8 @@
 --disable-jit \
 --disable-libffi \
 --enable-debug-symbols \
---with-llvmsrc=../tools/llvm/llvm3.6/ \
---with-llvmobj=../tools/llvm/llvm3.6/ \
+--with-llvmsrc=$DIR/../tools/llvm/llvm3.6/ \
+--with-llvmobj=$DIR/../tools/llvm/llvm3.6/ \
 --disable-optimized
 
 make -j16
