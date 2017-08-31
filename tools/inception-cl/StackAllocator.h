d7af499dd10c5c914183283ccd82ac94b834e07a
Corteggiani <nassim.corteggiani@maximintegrated.com>
2017-08-31 15:52:22 +0200

    StackAllocator.h manage .stack and sp

--- tools/inception-cl/StackAllocator.h
+++ tools/inception-cl/StackAllocator.h
@@ -10,7 +10,7 @@ class StackAllocator {
   static void Allocate(llvm::Module* mod, const Disassembler* Dis) {
     const object::SectionRef Section = Dis->getSectionByName(".stack");
 
-    Value* Reg = mod->getGlobalVariable("STACK");
+    Value* Reg = mod->getGlobalVariable(".stack");
 
     if (Reg == NULL) {
       Type* Ty = ArrayType::get(IntegerType::get(mod->getContext(), 4),
@@ -22,9 +22,39 @@ class StackAllocator {
           new GlobalVariable(*mod,   // Module
                              Ty,     // Type
                              false,  // isConstant
-                             GlobalValue::CommonLinkage, Initializer, "STACK");
+                             GlobalValue::CommonLinkage, Initializer, ".stack");
       gvar_ptr_SP->setAlignment(4);
     }
+  }
+
+  // XXX: SP cannot be initialized with other than 0
+  // XXX: Therefore, We introduce a store before the first
+  // instruction
+  static void InitSP(llvm::Module* mod, const Disassembler* Dis) {
+    const object::SectionRef Section = Dis->getSectionByName(".stack");
+
+    unsigned sp = Section.getAddress() + (Section.getSize() / 2);
+    ConstantInt* c_sp;
+
+    Value* Reg = mod->getGlobalVariable("SP");
+    if (Reg == NULL) {
+      Type* Ty = IntegerType::get(mod->getContext(), 32);
+
+      Constant* Initializer = Constant::getNullValue(Ty);
+
+      Reg = new GlobalVariable(*mod,   // Module
+                               Ty,     // Type
+                               false,  // isConstant
+                               GlobalValue::CommonLinkage, Initializer, "SP");
+    }
+
+    c_sp = ConstantInt::get(mod->getContext(), APInt(32, sp));
+
+    Function* main_fct = mod->getFunction("main");
+    Instruction* inst = main_fct->getEntryBlock().begin();
+
+    IRBuilder<>* builder = new IRBuilder<>(inst);
+    builder->CreateStore(c_sp, Reg);
   }
 };
 #endif
