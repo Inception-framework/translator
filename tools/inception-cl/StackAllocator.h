b1079daeb3bef5d7b0b1c9a3ec42e1c3c3ebd684
Giovanni Camurati <camurati@eurecom.fr>
2017-10-30 14:49:05 +0100

    use new API in StackAllocator

--- tools/inception-cl/StackAllocator.h
+++ tools/inception-cl/StackAllocator.h
@@ -1,6 +1,8 @@
 #ifndef STACK_ALLOCATOR_H
 #define STACK_ALLOCATOR_H
 
+#include "Utils/Builder.h"
+
 class StackAllocator {
  public:
   StackAllocator() {}
@@ -8,23 +10,11 @@ class StackAllocator {
   ~StackAllocator() {}
 
   static void Allocate(llvm::Module* mod, const Disassembler* Dis) {
+    // Main stack
     const object::SectionRef Section = Dis->getSectionByName(".stack");
-
-    Value* Reg = mod->getGlobalVariable(".stack");
-
-    if (Reg == NULL) {
-      Type* Ty = ArrayType::get(IntegerType::get(mod->getContext(), 4),
-                                Section.getSize());
-
-      Constant* Initializer = Constant::getNullValue(Ty);
-
-      GlobalVariable* gvar_ptr_SP =
-          new GlobalVariable(*mod,   // Module
-                             Ty,     // Type
-                             false,  // isConstant
-                             GlobalValue::CommonLinkage, Initializer, ".stack");
-      gvar_ptr_SP->setAlignment(4);
-    }
+    Type* Ty = ArrayType::get(IntegerType::get(mod->getContext(), 4),
+                              Section.getSize());
+    Reg(".stack", Ty);
   }
 
   // XXX: SP cannot be initialized with other than 0
@@ -36,25 +26,13 @@ class StackAllocator {
     unsigned sp = Section.getAddress() + (Section.getSize() / 2) - 4;
     ConstantInt* c_sp;
 
-    Value* Reg = mod->getGlobalVariable("SP");
-    if (Reg == NULL) {
-      Type* Ty = IntegerType::get(mod->getContext(), 32);
-
-      Constant* Initializer = Constant::getNullValue(Ty);
-
-      Reg = new GlobalVariable(*mod,   // Module
-                               Ty,     // Type
-                               false,  // isConstant
-                               GlobalValue::CommonLinkage, Initializer, "SP");
-    }
-
     c_sp = ConstantInt::get(mod->getContext(), APInt(32, sp));
 
     Function* main_fct = mod->getFunction("main");
     Instruction* inst = main_fct->getEntryBlock().begin();
 
     IRBuilder<>* builder = new IRBuilder<>(inst);
-    builder->CreateStore(c_sp, Reg);
+    builder->CreateStore(c_sp, Reg("SP"));
   }
 };
 #endif
